9e62bdb8c383a508091d92ad7a51e920614c04e4
Joe <joebreilly@yahoo.ie>
2021-05-12 16:42:20 +0000

    Event listeners added to dom elements in assets/main.js

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("irradience").addEventListener("change", computeSavings);
+document.getElementById("roof_space").addEventListener("change", computeSavings);
+document.getElementById("unit_cost").addEventListener("change", computeSavings);
+document.getElementById("orientation").addEventListener("change", computeSavings);
