4a4d9e92189ab5fcf23be86a2ee0b03b908786f1
Joe <joebreilly@yahoo.ie>
2021-05-29 13:43:57 +0000

    Google maps javascript code added to maps.js

---
+++ assets/js/maps.js
@@ -0,0 +1,31 @@
+function initMap() {
+    // Map options
+    var options = {
+        zoom: 8,
+        center: {
+            lat: 53.7276,
+            lng: -7.7933
+        }
+    }
+    // New map
+    var map = new google.maps.Map(document.getElementById("map"), options);
+
+    // Add marker
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 53.5644,
+            lng: -7.7646
+        },
+        map: map
+    });
+
+    // 
+    var infoWindow = new google.maps.InfoWindow({
+        content: "<h3>SolaNew PV Systems</h3>"
+    });
+
+    // Add event listener to marker 
+    marker.addListener("click", function () {
+        infoWindow.open(map, marker);
+    });
+}
